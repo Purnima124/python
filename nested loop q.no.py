404b4046b2d0b5ae71fce03e7de9ac1efebc19e0
Purnima124 <82706192+Purnima124@users.noreply.github.com>
2022-02-27 15:38:47 +0530

    Add files via upload

---
+++ nested loop q.no.py
@@ -0,0 +1,33 @@
+# # i=0
+# # while i<5:
+# #     j=0
+# #     while j<5:
+# #         if j>3:
+# #             break
+# #         else:
+# #             print("*")
+# #             j=j+1
+# #     print("*")
+# #     i=i+1
+# x=0
+# while x < 7:
+#     if x==3 or x==5:
+#         x=x+1
+#         continue
+#     print(x)
+#     x=x+1
+k=1
+i=1
+while i<=5:
+    a=1
+    while a<=5-i:
+        print("*",end ="*")
+        a=a+1
+print()
+# j=1
+# while j<=k:
+#     print("*",end="")
+#     j=j+1
+# k=k+2
+# print(" ")
+# i=i+2
