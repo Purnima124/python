3ec72334042ddd18d7c679b663caacbb1487d8db
Purnima124 <82706192+Purnima124@users.noreply.github.com>
2022-02-27 15:33:12 +0530

    Add files via upload

---
+++ words.py
@@ -0,0 +1,28 @@
+import string
+import random
+
+def load_words():
+    """
+    Ye function kaafi jayada words ko load karne mai help karega
+    """
+    # WORDLIST_FILENAME="words.tex"
+    # inFile=open(WORDLIST_FILENAME,'r')
+    # line=inFile.readline()
+    # word_list=string.split(line)
+    # return word_list
+    word_list=["navgurukul","learning","kindness"]
+    return word_list
+
+def choose_word():
+    """
+    word_list (list): list of words (strings)
+    ye function ek word randomly return karega
+    """
+    word_list = load_words()
+    secret_word = random.choice(word_list)
+    secret_word = secret_word.lower()
+
+    return secret_word
+    print(secret_word)
+
+    
